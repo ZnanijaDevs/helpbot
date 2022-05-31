073611d80c132626ecdf26a388da812000070ea0
southpaw203 <73884858+vlaex@users.noreply.github.com>
2022-05-31 23:09:18 +0300

    Update templates, add launch.json

--- website/routes/homepage.py
+++ website/routes/homepage.py
@@ -4,6 +4,7 @@ import pytz
 from fastapi.requests import Request
 from fastapi.responses import HTMLResponse
 from website.app import app, templates
+from bot.database import redis
 
 
 @app.get('/', response_class=HTMLResponse)
@@ -12,11 +13,14 @@ async def homepage(request: Request):
     mode = os.environ['MODE']
     source_url = 'https://github.com/vlaex/helpbot-spamouts'
 
+    matches = redis.keys('todelete:*')
+
     context = {
         'request': request,
         'server_time': now.strftime('%m/%d/%Y, %H:%M:%S'),
         'mode': mode,
-        'source_url': source_url
+        'source_url': source_url,
+        'messages_count': len(matches)
     }
 
     return templates.TemplateResponse(
